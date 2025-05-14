76c39b86d5272edee73f4295b4cf4acc2c73d2c1
name22334 <quang20042204@gmail.com>
2025-05-14 13:43:24 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const routes = Router();
 
-// routes.use("/products", hanldeProduct...)
+routes.use("/products", console.log("ddd"))
 // routes.use("/products", hanldeProduct...)
 // routes.use("/products", hanldeProduct...)
 // routes.use("/products", hanldeProduct...)
